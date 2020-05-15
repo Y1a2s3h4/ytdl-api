e26d5bac73f5a13d6627c45d5c7173b6aeafeb7f
Y1a2s3h4 <yashpurohit763@gmail.com>
2020-05-15 16:52:43 +0530

    added cors

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  cors = require("cors");
+
+app.use(cors());
 
 app.use("/v", require("./Route/app"));
 
 app.use((err, req, res, next) => res.status(err.statusCode).send(err));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
